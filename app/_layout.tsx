e98420ee5554f04a123adf0a9a0dad63c5cf1699
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-10-06 16:38:17 +0900

    feat: add bottom tab navigator

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "HOME" }} />
-      <Stack.Screen name="about" options={{ title: "ABOUT" }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
