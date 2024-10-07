2bb62c1c6b5c34d1e3702c6a8dcb0ed63cc53972
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-10-08 08:26:36 +0900

    feat: add sticker to photo

--- components/EmojiList.tsx
+++ components/EmojiList.tsx
@@ -9,34 +9,34 @@ type Props = {
 
 export default function EmojiList({ onSelect, onCloseModal }: Props) {
   const [emoji] = useState([
-    require("@/assets/images/emoji1.png"),
-    require("@/assets/images/emoji2.png"),
-    require("@/assets/images/emoji3.png"),
-    require("@/assets/images/emoji4.png"),
-    require("@/assets/images/emoji5.png"),
-    require("@/assets/images/emoji6.png"),
+    require("../assets/images/emoji1.png"),
+    require("../assets/images/emoji2.png"),
+    require("../assets/images/emoji3.png"),
+    require("../assets/images/emoji4.png"),
+    require("../assets/images/emoji5.png"),
+    require("../assets/images/emoji6.png"),
   ]);
 
-  console.log("hiihih");
   return (
     <FlatList
       horizontal
       showsHorizontalScrollIndicator={Platform.OS === "web"}
       data={emoji}
       contentContainerStyle={styles.listContainer}
-      renderItem={({ item, index }) => {
-        console.log("item: ", item);
-        return (
-          <Pressable
-            onPress={() => {
-              onSelect(item);
-              onCloseModal();
-            }}
-          >
-            <Image source={item} key={index} style={styles.image} />
-          </Pressable>
-        );
-      }}
+      renderItem={({ item, index }) => (
+        <Pressable
+          onPress={() => {
+            onSelect(item);
+            onCloseModal();
+          }}
+        >
+          <Image
+            source={require("../assets/images/emoji1.png")}
+            key={index}
+            style={styles.image}
+          />
+        </Pressable>
+      )}
     />
   );
 }
