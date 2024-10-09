ddf2fc4944389ba36d114f848af8c83577e4ade8
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-10-09 21:46:14 +0900

    feat: implement debounced search images

--- app/(tabs)/photos.tsx
+++ app/(tabs)/photos.tsx
@@ -1,23 +1,53 @@
-import { useState } from "react";
-import { FlatList, View, StyleSheet, Pressable, Text } from "react-native";
+import { useState, useMemo } from "react";
+import {
+  TextInput,
+  FlatList,
+  View,
+  StyleSheet,
+  Pressable,
+  Text,
+} from "react-native";
+import { useQuery } from "@tanstack/react-query";
 import { Image } from "expo-image";
 import { searchImages } from "@/api/imagesService";
 import { ImageResponse } from "@/types/images";
+import debounce from "lodash.debounce";
 
 export default function PhotosScreen() {
-  const [images, setImages] = useState<ImageResponse[]>([]);
-  const onClick = async () => {
-    const imagesData = await searchImages("car");
-    setImages(imagesData);
+  const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+
+  const debouncedSearch = useMemo(
+    () =>
+      debounce((input: string) => {
+        setDebouncedQuery(input);
+      }, 500),
+    []
+  );
+
+  // TextInput에서 검색어가 변경될 때마다 debouncedSearch 호출
+  const handleChangeText = (input: string) => {
+    setQuery(input);
+    debouncedSearch(input);
   };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["search", debouncedQuery],
+    queryFn: () => searchImages(debouncedQuery),
+    staleTime: 300000, // 5분 동안 데이터가 stale되지 않음
+  });
 
   return (
     <View style={styles.container}>
-      <Pressable onPress={onClick}>
-        <Text style={styles.text}>Search</Text>
-      </Pressable>
+      <TextInput
+        style={styles.input}
+        placeholder="검색어를 입력하세요"
+        value={query}
+        onChangeText={handleChangeText}
+      />
+      <Text style={styles.searchTerm}>"{debouncedQuery}"</Text>
       <FlatList
-        data={images}
+        data={data}
         renderItem={({ item }) => (
           <Image source={item.urls.small_s3} style={styles.image} />
         )}
@@ -33,6 +63,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     justifyContent: "center",
     alignItems: "center",
+  },
+  input: {
+    height: 40,
+    color: "#fff",
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  searchTerm: {
+    fontSize: 40,
+    color: "#fff",
+    marginBottom: 20,
   },
   text: {
     color: "#fff",
